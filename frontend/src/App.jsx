26ca03dc7993304744ab7d3a9f27492a42a7b1d6
Anam-Ashraf7 <anam.ashraf.7357@gmail.com>
2024-03-13 15:33:19 +0530

    Created landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-
+import Home from "./components/Home"
 
 function App() {
 
   return (
     <>
-      <div className="flex items-center justify-center h-[100dvh] font-raleway">
-        <h1 className="text-5xl">Welcome to WeCollab</h1>
-      </div>
+      <Home/>
     </>
   )
 }
