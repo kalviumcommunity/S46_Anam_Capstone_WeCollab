4d8beec81d56eee535f350f95f20e5b343fc85f0
Anam-Ashraf7 <anam.ashraf.7357@gmail.com>
2024-03-13 16:38:27 +0530

    Changed src for deployment

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     <>
         <nav className="border-black border-b-2 flex justify-between items-center p-3">
             <div className="flex items-center gap-3 cursor-pointer">
-                <img className="size-10 lg:size-[inherit]" src="../../public/logo.svg" alt="Logo of WeCollab" />
+                <img className="size-10 lg:size-[inherit]" src="./assets/logo.svg" alt="Logo of WeCollab" />
                 <h1 className="text-2xl lg:text-3xl font-raleway font-bold pt-1">WeCollab</h1>
             </div>
             <div className="hidden md:flex lg:flex gap-10 font-space text-xl [&_*]:cursor-pointer">
@@ -15,7 +15,7 @@ export default function Navbar() {
             <div className="hidden lg:block text-xl text-white font-raleway font-bold">
                 <button className="rounded-full bg-orange-600 px-5 py-1">+ Join</button>
             </div>
-            <img className="md:hidden lg:hidden" src="../../public/responsive-nav.svg" alt="Responsive navigation icon" />
+            <img className="md:hidden lg:hidden" src="./assets/responsive-nav.svg" alt="Responsive navigation icon" />
         </nav>
     </>
   )
