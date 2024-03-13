6da928f70d9df766cb27c9f605f57bf9507b5f30
Anam-Ashraf7 <anam.ashraf.7357@gmail.com>
2024-03-13 15:59:50 +0530

    Created components for navbar and footer and added alt for img

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import logo from "../assets/logo.svg"
+
+export default function Footer() {
+  return (
+    <>
+        <footer className="flex flex-col-reverse gap-10 lg:flex-row lg:gap-0 items-center justify-between p-10">
+            <div className="flex flex-col items-center lg:items-start gap-5">
+                <div className="flex gap-3 items-center cursor-pointer">
+                    <img className="size-8 lg:size-[inherit]" src={logo} alt="Logo of WeCollab" />
+                    <h1 className="text-xl lg:text-3xl font-raleway font-bold pt-3">WeCollab</h1>
+                </div>
+                <p className="hidden lg:block font-space w-3/4">Collaborate, Innovate, Thrive with WeCollab</p>
+                <p className="font-raleway">Created by <span className="underline italic cursor-pointer">Anam Ashraf</span></p>
+            </div>
+            <div className="flex gap-20">    
+                <div className="font-raleway">
+                    <h1 className="font-bold py-2">Quick Links</h1>
+                    <ul className="[&_*]:cursor-pointer">
+                        <li className="hover:underline">People</li>
+                        <li className="hover:underline">Projects</li>
+                        <li className="hover:underline">Open Ideas</li>
+                    </ul>
+                </div>
+                <div className="font-raleway">
+                    <h1 className="font-bold py-2">How it works</h1>
+                    <ul>
+                        <li className="hover:underline">FAQ</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="hidden lg:flex flex-col items-end gap-5 font-raleway">
+                <h1 className="text-2xl w-3/4 text-right">Are you excited to collaborate?</h1>
+                <button className="bg-orange-600 text-white px-4 py-2 w-1/3 font-bold">Join Now</button>
+            </div>
+        </footer>
+    </>
+  )
+}
