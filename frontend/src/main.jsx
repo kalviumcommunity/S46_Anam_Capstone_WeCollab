10fbb54ac48063d6e80c887fd6b5fe8fe4d24a29
Anam-Ashraf7 <anam.ashraf.7357@gmail.com>
2024-03-22 16:28:03 +0530

    Generated refresh token for users and setup foundation for authentication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const getCookie = (cookieName) => {
+  const cDecoded = decodeURIComponent(document.cookie)
+  const cArray = cDecoded.split("; ")
+  console.log(cArray)
+  let result;
+
+  cArray.map(cookie => {
+    if(cookie.indexOf(cookieName) === 0){
+      result = cookie.substring(cookieName.length+1)
+    }
+  })
+
+  return result
+}
+
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_APOLLO_URI,
+});
+
+const authLink = setContext((operation, previousContext) => {
+  if (operation.operationName.startsWith("update") || operation.operationName.startsWith("delete")){
+    const token = getCookie('token');
+    return {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    }
+  }
+});
 
 const client = new ApolloClient({
-  uri: import.meta.env.VITE_APOLLO_URI,
-  cache: new InMemoryCache()
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  credentials: "include"
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
