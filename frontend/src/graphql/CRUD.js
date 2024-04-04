a8f12f0564e213c2d0da8feea8d9407897b9438e
Anam-Ashraf7 <anam.ashraf.7357@gmail.com>
2024-04-04 16:18:47 +0530

    Implemented refresh token rotation

--- frontend/src/graphql/CRUD.js
+++ frontend/src/graphql/CRUD.js
@@ -1,17 +1,17 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_USER = gql `
-    mutation CreateUser($userInput: addUser!){
-        createUser(userInput: $userInput){
+export const USER_SIGNUP = gql `
+    mutation UserSignup($userInput: addUser!){
+        userSignup(userInput: $userInput){
             id
             email
             token
     }
 }
 `
-export const LOGIN_USER = gql `
-    mutation LoginUser($loginData: loginInput!){
-        loginUser(loginData: $loginData){
+export const USER_LOGIN = gql `
+    mutation UserLogin($loginData: loginInput!){
+        userLogin(loginData: $loginData){
             id
             email
             token
